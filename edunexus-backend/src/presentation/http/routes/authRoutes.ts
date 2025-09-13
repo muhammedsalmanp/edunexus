be14a024a505ec0914e1cb6d05e83211bdc0b3eb
muhammedsalmanp <151722344+muhammedsalmanp@users.noreply.github.com>
2025-09-14 01:36:12 +0530

    refresh-token done

--- edunexus-backend/src/presentation/http/routes/authRoutes.ts
+++ edunexus-backend/src/presentation/http/routes/authRoutes.ts
@@ -15,32 +15,40 @@ import { LoginUseCase } from "../../../app/useCase/auth/LoginUseCase";
 import { EmailService } from "../../../infrastructure/service/sendOtpEmail";
 import { TokenService } from "../../../infrastructure/service/TokenService";
 import { AuthController } from "../../Controller/AuthController";
+import { RefreshTokenUseCase } from "../../../app/useCase/RefreshTokenUseCase";
+import { refreshTokenMiddleware } from "../../../infrastructure/middleware/refreshTokenMiddleware";
+import { RefreshTokenController } from "../../Controller/RefreshTokenController ";
+import { RefreshTokenAdapter } from "../../adapters/RefreshTokenAdapter";
 
-const route = Router();
+const router = Router();
 
 
-const _useRepo = new userRepository();
-const _OtpRepo = new otpRepository();
-const _emailService = new EmailService();
-const _tokenService = new TokenService();
+const useRepo = new userRepository();
+const OtpRepo = new otpRepository();
+const emailService = new EmailService();
+const tokenService = new TokenService();
 
-const _registerTeacherUseCase = new RegisterTeacherUseCase(_useRepo);
-const _registerUserUseCase = new RegisterUserUseCase(_useRepo);
-const _registerAdminUseCase = new RegisterAdminUseCase(_useRepo);
-const _authController = new AuthController(_registerTeacherUseCase, _registerUserUseCase, _registerAdminUseCase);
-const _LoginUseCase = new  LoginUseCase(_useRepo , _tokenService);
-const _SendOtpUseCase = new SendOtpUseCase(_OtpRepo,_emailService);
-const _VerifyOtpUseCase = new VerifyOtpUseCase(_OtpRepo, _useRepo);
-const _ForgotPasswordUseCase = new ForgotPasswordUseCase(_OtpRepo,_emailService);
-const _ResetPasswordUseCase = new ResetPasswordUseCase(_useRepo, _OtpRepo);
+const registerTeacherUseCase = new RegisterTeacherUseCase(useRepo);
+const registerUserUseCase = new RegisterUserUseCase(useRepo);
+const registerAdminUseCase = new RegisterAdminUseCase(useRepo);
+const authController = new AuthController(registerTeacherUseCase, registerUserUseCase, registerAdminUseCase);
+const loginUseCase = new  LoginUseCase(useRepo , tokenService);
+const sendOtpUseCase = new SendOtpUseCase(OtpRepo,emailService);
+const verifyOtpUseCase = new VerifyOtpUseCase(OtpRepo, useRepo);
+const forgotPasswordUseCase = new ForgotPasswordUseCase(OtpRepo,emailService);
+const resetPasswordUseCase = new ResetPasswordUseCase(useRepo, OtpRepo);
+const refreshTokenUseCase = new RefreshTokenUseCase(tokenService)
 
-route.post('/register/student', adaptRegisterRoute(_authController, 'student'));
-route.post('/register/teacher', adaptRegisterRoute(_authController, 'teacher'));
-route.post('/register/admin', adaptRegisterRoute(_authController, 'admin'));
-route.post('/login', adaptRoute(_LoginUseCase));
-route.post('/send-otp', adaptRoute(_SendOtpUseCase));
-route.post('/verify-otp', adaptRoute(_VerifyOtpUseCase));
-route.post('/forgot-password', adaptRoute(_ForgotPasswordUseCase));
-route.post('/reset-password', adaptRoute(_ResetPasswordUseCase));
+const refreshTokenAdapter = new RefreshTokenAdapter(refreshTokenUseCase)
 
-export default route;
+router.post('/register/student', adaptRegisterRoute(authController, 'student'));
+router.post('/register/teacher', adaptRegisterRoute(authController, 'teacher'));
+router.post('/register/admin', adaptRegisterRoute(authController, 'admin'));
+router.post('/login', adaptRoute(loginUseCase));
+router.post('/send-otp', adaptRoute(sendOtpUseCase));
+router.post('/verify-otp', adaptRoute(verifyOtpUseCase));
+router.post('/forgot-password', adaptRoute(forgotPasswordUseCase));
+router.post('/reset-password', adaptRoute(resetPasswordUseCase));
+router.post('/refresh-token', refreshTokenMiddleware,  refreshTokenAdapter.execute.bind(refreshTokenAdapter))
+
+export default router;
